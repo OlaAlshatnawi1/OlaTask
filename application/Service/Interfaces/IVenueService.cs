c00a15de90f664281ba4ee47ff10fd0efd6dab46
OlaAlshatnawi1 <o.AlShatnawi@penguinin.com>
2026-04-13 10:50:08 +0300

    clean some comments

--- application/Service/Interfaces/IVenueService.cs
+++ application/Service/Interfaces/IVenueService.cs
@@ -8,9 +8,9 @@ namespace application.Service.Interfaces
     public interface IVenueService
     {
         List<VenueDto> GetAll(VenueFilter filter = null);
-        VenueDto GetById(int id);                          // throws NotFoundException if missing
-        Venue Create(CreateVenueRequest request);           // ← request DTO now
-        Venue Update(int id, UpdateVenueRequest request);  // ← request DTO now
+        VenueDto GetById(int id);                         
+        Venue Create(CreateVenueRequest request);          
+        Venue Update(int id, UpdateVenueRequest request); 
         Task<bool> DeleteVenue(int id);
 
 
